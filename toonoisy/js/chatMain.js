c5876c370212c4e73fe08eec3f419fda596358de
wDwWb <760224261@qq.com>
2018-11-12 20:20:45 +0800

    实现创建房间的功能

--- toonoisy/js/chatMain.js
+++ toonoisy/js/chatMain.js
@@ -1,4 +1,4 @@
-function verification(){
+﻿function verification(){
     var fPassword = document.getElementById("newPassword").value
     var sPassword = document.getElementById("aNewPassword").value
 
@@ -31,8 +31,31 @@ function changecolor() {
     
 }
 
-// $("ul li").mouseover(function(){
-//     $(this).stop().animate({height:114},200);
-//     $(this).siblings().stop().animate({height:34},200);
-// })
+/***
+ * 动态增加tabs标签页
+ */
+function addTabFunction(){
+   
+    var tabTitle = $("#tab_Title"),//获取房间名
+    //创建房间的主文id
+    tabCounter=2,
+    //标签页格式
+    tabTemplate = "<li><a href='#{href}' data-toggle='tab'  >#{tabtitle}<span class='glyphicon glyphicon-remove' style='cursor:pointer;'></span></a></li>";
+
+    //ul id mytab  div id screen
+    var tabtitle = tabTitle.val();
+    var div_pane = tabtitle+'_screen';
+    document.getElementById("screen").innerHTML+="<div id='"+div_pane +"' class='tab-pane'>yoyoy</div>"
+    document.getElementById("mytab").innerHTML+="<li  id='"+ tabtitle +"'><a href='#"+div_pane + "' data-toggle='tab'  >"+tabtitle+"<span class='glyphicon glyphicon-remove' style='cursor:pointer;' onclick='removeTab(parentElement.parentElement.id)'></span></a></li>";
+
+
+}
+/**
+ * 关闭标签页 
+ */
+function removeTab(roomName){
+
+    document.getElementById(roomName).remove();
+    document.getElementById(roomName+"_screen").remove();
+}
 
